a58eeef9c56f0eabb0e5cd5773ff6f0e77262dc0
leythadan <16468186+leythadan@users.noreply.github.com>
2022-12-09 02:11:39 -0800

    omment

---
+++ Makefile
@@ -0,0 +1,15 @@
+
+all: add commit push
+
+status:
+	git status
+
+add:
+	git add .
+
+commit:
+	@read -p "comments :" comment
+	git commit -m "$comment"
+
+push:
+	git push
--- perl/swap_variable_val.pl
+++ perl/swap_variable_val.pl
@@ -6,9 +6,9 @@ use warnings;
 my $var1 = 5;
 my $var2 = 10;
 
-print"Before Swap: var1 => $var1, var2 => $var2 \n";
+print "Before Swap: var1 => $var1, var2 => $var2 \n";
 
 # swap variable values
 ($var1, $var2) = ($var2, $var1);
 
-print"After Swap: var1 => $var1, var2 => $var2 \n";
+print "After Swap: var1 => $var1, var2 => $var2 \n";
