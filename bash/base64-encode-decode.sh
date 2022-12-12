d2e4d62c586bc1f1dffb6b46676149fc39692de3
Leyth <16468186+leythadan@users.noreply.github.com>
2022-12-11 23:42:35 -0800

    encode and ecode base64

--- bash/base64-encode-decode.sh
+++ bash/base64-encode-decode.sh
@@ -1,3 +1,27 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 
-echo "Select from the following options : "
+echo "Select an option ?"
+echo "Option 1: encode base64"
+echo "Option 2: decode base64"
+
+read option
+
+case $option in
+1)
+   read -p "Enter username " username
+   read -sp "Enter password " password
+   echo
+   creds=$(echo -n "$username:$password" | base64);
+   echo "Your base64 encoded string is : $creds";;
+2)
+    read -p "Enter string to decode " string
+    creds=$(echo -n  "$string"| base64  --decode)
+    echo "$creds" | awk -F':' '{print "username: "$1, "\npassword: " $2}';;
+*)
+    echo "not a valid option";;
+esac
+
+
+
+
+
